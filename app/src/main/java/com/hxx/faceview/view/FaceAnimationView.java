24cab96e48a363a19e31ed7c11794bf6e8ac2c57
houxx <hou_xiangxiang@126.com>
2017-03-09 19:48:11 +0800

    ttt# Please enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/com/hxx/faceview/view/FaceAnimationView.java
@@ -0,0 +1,12 @@
+package com.hxx.faceview.view;
+
+/**
+ * <ul>
+ * <li>功能说明：</li>
+ * <li>作者：tal on 2017/3/1 0001 12:17 </li>
+ * <li>邮箱：houxiangxiang@cibfintech.com</li>
+ * </ul>
+ */
+
+public class FaceAnimationView {
+}
